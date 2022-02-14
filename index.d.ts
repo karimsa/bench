ca0c1d10c3da6ff1396fdf1ee699fc28b07c78ec
Karim Alibhai <karim@alibhai.co>
2022-02-14 16:29:39 -0500

    add types

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module '@karimsa/bench' {
+    export interface BenchmarkConfig {
+        growthFn?: (index: number) => number
+        benchTime?: number
+        minIterations?: number
+        maxIterations?: number
+        forceExit?: boolean
+        perfHooks?: boolean
+    }
+
+    export interface BenchmarkHelper {
+        N(): number
+        runConcurrently(runner: (iteration: number) => Promise<void>): Promise<void>
+        timeSync<T>(name: string, handler: () => T): T
+        timeAsync<T>(name: string, handler: () => Promise<T>): T
+        resetTimer(): void
+        stopTimer(): void
+    }
+
+    type CreateBenchmarkUtil = (
+        title: string,
+        handler: (b: BenchmarkHelper) => Promise<void>,
+        config?: BenchmarkConfig
+    ) => void
+
+    export const benchmark: CreateBenchmarkUtil & {
+        only: CreateBenchmarkUtil
+    }
+}
+
